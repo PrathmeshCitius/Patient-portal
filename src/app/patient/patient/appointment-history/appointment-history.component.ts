d0bb7fcf8748ec27944debb5ff94db6e5f740c3b
Dipali Tiwaskar <DipaliT@citiustech.com>
2022-02-17 13:21:21 +0530

    appointment history component

--- src/app/patient/patient/appointment-history/appointment-history.component.ts
+++ src/app/patient/patient/appointment-history/appointment-history.component.ts
@@ -4,6 +4,7 @@ import { ApiService } from '../../services/services';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-appointment-history',
   templateUrl: './appointment-history.component.html',
@@ -11,18 +12,33 @@ import {MatTableDataSource} from '@angular/material/table';
 })
 export class AppointmentHistoryComponent implements OnInit {
 
-  displayedColumns: string[] = ['vaccineName', 'doses', 'date', 'action'];
+  displayedColumns: string[] = ['apppintmentDate', 'physician', 'status', 'notes'];
   dataSource!: MatTableDataSource<any>;
   onedit=false;
   onAdd=true;
+  aId;
 
   @ViewChild(MatPaginator) paginator! : MatPaginator;
   @ViewChild(MatSort) sort! : MatSort;
 
   
-  constructor(private api:ApiService) { }
+  constructor(private api:ApiService, private router: Router, private activatedRoute:ActivatedRoute) {
+    
+   }
 
   ngOnInit(): void {
+    this.getAppoHistory();
   }
 
+  getAppoHistory(){
+    this.api.getAppointmentHistory().subscribe(res=>{
+      console.log("appointment History Resp", res)
+      this.dataSource=res;
+      console.log("app history datasource", this.dataSource)
+    })
+  }
+  // onViewClick(){
+  //   this.router.navigate['appointment-history/appointment-detail']
+  //   console.log("on view click function")
+  // }
 }
--- src/app/patient/services/services.ts
+++ src/app/patient/services/services.ts
@@ -44,4 +44,13 @@ updateUserById(id:any, data:any){
     return this.http.put<any>("http://localhost:3000/users/"+ id, data)
 }
 
+//code for appointment history
+
+getAppointmentHistory(){
+    return this.http.get<any>("http://localhost:3000/appointment-history/")
+}
+getDetailById(id:any){
+    return this.http.get("http://localhost:3000/appointment-history/"+id)
+    
+}
 }
